02cc0754c4e9b54bd1a1591584d9bbabed68aec3
Christopher Wahl <christopher@nothingmuch.net>
2021-07-03 14:13:05 -0400

    Initial config and templating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function NumberButton({textId, value, clickListener}) {
+    const emit = () => clickListener(value);
+    return (
+        <button className="number" id={ textId } onClick={ emit } >{ value }</button>
+    );
+}
+
+function OpButton({textId, value, clickListener}) {
+    const emit = () => clickListener(value);
+    return (
+        <button className="operaton" id={ textId } onClick={ emit } >{ value }</button>
+    );
+}
+
+class App extends React.Component {
+    render() {
+        const numbers = [
+            'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+        ].map((textId, value) => (
+            <NumberButton key={value} textId={textId} value={value + 1} clickListener={null} />
+        ));
+        return (
+            <div className="App">
+                <header id="calc-container">
+                    { numbers.slice(6 ) }
+                    <OpButton value="-" textId="minus" clickListener={null} />
+                    { numbers.slice(3, 6) }
+                    <OpButton value="+" textId="plus" clickListener={null} />
+                    { numbers.slice(0, 3) }
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
