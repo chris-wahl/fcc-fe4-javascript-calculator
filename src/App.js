78e7e89dd3801afc64c74024e673e44e724f971c
Christopher Wahl <christopher@nothingmuch.net>
2021-07-04 18:07:16 -0400

    Operationally complete

--- src/App.js
+++ src/App.js
@@ -17,10 +17,7 @@ function NumberButton({textId, value, clickListener, className = []}) {
 }
 
 function OpButton({textId, value, clickListener, className = []}) {
-    const emit = (e) => {
-        console.log('emit', e);
-        clickListener(value);
-    };
+    const emit = () => clickListener(value);
     const classes = [
         'operations', ...className
     ];
@@ -31,33 +28,36 @@ function Display({readout, id}) {
     return (<div id={id}>{readout}</div>);
 }
 
+const initState = {
+    value: null,
+    operations: ['0']
+}
+
 class App extends React.Component {
     numbers = [];
 
     constructor() {
         super();
-        this.state = {
-            operations: ['0']
-        };
         this.addNumber = this.addNumber.bind(this);
         this.addDecimal = this.addDecimal.bind(this);
         this.addOperation = this.addOperation.bind(this);
         this.performCalculation = this.performCalculation.bind(this);
+        this.clear = this.clear.bind(this);
 
         this.getValues = this.getValues.bind(this);
         this.setValue = this.setValue.bind(this);
 
-        this.numbers = [
-            'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
-        ].map((textId, value) => (
-            <NumberButton key={value} textId={textId} value={value + 1} clickListener={this.addNumber}/>
-        ));
+        this.numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].map(
+            (textId, value) =>
+                (<NumberButton key={value.toString()} textId={textId} value={(value + 1).toString()}
+                               clickListener={this.addNumber}/>)
+        );
+        this.state = JSON.parse(JSON.stringify(initState));
+        this.clear();
     }
 
     clear() {
-        this.setState({
-            operations: ['0']
-        })
+        this.setState(JSON.parse(JSON.stringify(initState)))
     }
 
     getValues() {
@@ -75,26 +75,60 @@ class App extends React.Component {
     }
 
     addNumber(v) {
-        console.log('add number', v);
+        if (this.state.value !== null) {
+            this.clear();
+            return setTimeout(() => this.addNumber(v));
+        }
 
         const {operations, lastValue} = this.getValues();
 
-        if (lastValue === '0') {
-            if (v === 0) {
-                return;
+        if (operations.length === 1) {
+            // Only one thing in the queue.
+            console.log('operations', operations);
+            if (lastValue === '0') {
+                // First entry is "init'd" - replace it.
+                return this.setValue(v);
+            } else if (lastValue === '-') {
+                // First entry is a negation operation.  Store the negated number.
+                return this.setValue(`-${v}`);
             }
-            v = v.toString();
-        } else if (OPERATORS.some(s => lastValue.endsWith(s))) {
-            operations.push(v.toString());
+            // First number is numeric.  Append the next digit.
+            return this.setValue(`${lastValue}${v}`);
+        }
+
+        if (OPERATORS.some(s => lastValue.endsWith(s))) {
+            // Previous value was an operation.  Check if it's a negation operation (i.e "+-", "--", "/-", or "*-")
+            if (lastValue.length > 1) {
+                // Operation is a negation operation.  Remove the extra minus sign from the operation and replace it.
+                // Use it to set the input number negative
+                v = `-${v}`
+                this.setValue(lastValue[0]);
+            }
+            // Push the new numeric value into the array and set the state.
+            operations.push(v);
             return this.setState({operations});
         } else {
+            // Append the number.
+            if (lastValue === '0') {
+                return this.setValue(v);
+            }
             v = `${lastValue}${v}`;
+            this.setValue(v)
         }
-        this.setValue(v)
-        console.log(operations);
     }
 
     addDecimal() {
+        if (this.state.value !== null) {
+            this.setState({
+                value: null,
+                operations: [this.state.value.toString()]
+            })
+            return setTimeout(() => {
+                this.addDecimal();
+            });
+        }
+
+
         const {operations, lastValue} = this.getValues();
         console.log(operations, lastValue);
         if (lastValue.includes('.')) {
@@ -107,28 +141,88 @@ class App extends React.Component {
     }
 
     addOperation(v) {
-        // TODO: Allow second "minus" sign to make a number negative
+        if (this.state.value !== null) {
+            this.setState({
+                value: null,
+                operations: [this.state.value.toString()]
+            })
+            return setTimeout(() => this.addOperation(v));
+        }
         const {operations, lastValue} = this.getValues();
+
+        //
+        if (operations.length === 1 && (lastValue === '0' || lastValue === '-')) {
+            if (v !== '-') {
+                // Init state and not trying to negate the value.  Don't do anything.
+                return;
+            }
+            // Init state and trying to start out with a negative number.  Set it as the state.
+            return this.setValue('-');
+        }
+
+        // most recent value is an operator
         if (OPERATORS.some(s => lastValue.endsWith(s))) {
+            if (v === '-') {
+                if (lastValue.length === 1) {
+                    // Setting the following number to negative.
+                    return this.setValue(`${lastValue}-`);
+                } else if (lastValue.length === 2) {
+                    // Just keep on hitting '-'... do nothing. It's already set.
+                    return;
+                }
+            }
+            // Replace the current operation.
             this.setValue(v);
         } else {
+            // Most recent value is a number.  Push the new operation.
             operations.push(v);
             this.setState({operations});
         }
     }
 
     performCalculation() {
+        if (this.state.value !== null) {
+            return;
+        }
+        const operations = this.state.operations;
 
+        let acc = parseFloat(operations[0]);
+
+        for (let i = 1; i < operations.length - 1; i += 2) {
+            const operation = operations[i];
+            const nextValue = parseFloat(operations[i + 1]);
+            switch (operation) {
+                case '+':
+                    acc += nextValue
+                    break;
+                case '-':
+                    acc -= nextValue;
+                    break;
+                case 'x':
+                    acc *= nextValue;
+                    break;
+                case '/':
+                    acc /= nextValue;
+                    break;
+                default:
+            }
+        }
+        operations.push(`=${acc}`);
+        this.setState({
+            value: acc,
+            operations
+        });
     }
 
     render() {
         return (
             <div className="App">
                 <header id="calc-container">
-                    <Display readout={this.state.operations.join('')} id="readout"/>
-                    <Display readout={this.state.operations[this.state.operations.length - 1]} id="display"/>
-                    <OpButton value="AC" textId="clear" clickListener={this.clear.bind(this)}
-                              className={["two-column"]}/>
+                    <Display readout={this.state.operations.join(' ')} id="readout"/>
+                    <Display readout={this.state.value ?? this.state.operations[this.state.operations.length - 1]}
+                             id="display"/>
+                    <OpButton value="AC" textId="clear" clickListener={this.clear}
+                              className={["two-column", "ac-button"]}/>
                     <OpButton value="/" textId="divide" clickListener={this.addOperation}/>
                     <OpButton value="x" textId="multiply" clickListener={this.addOperation}/>
                     {this.numbers.slice(6)}
@@ -136,9 +230,9 @@ class App extends React.Component {
                     {this.numbers.slice(3, 6)}
                     <OpButton value="+" textId="add" clickListener={this.addOperation}/>
                     {this.numbers.slice(0, 3)}
-                    <OpButton value="=" textId="equals" className={["two-row"]}
+                    <OpButton value="=" textId="equals" className={["two-row", "equals-button"]}
                               clickListener={this.performCalculation}/>
-                    <NumberButton key={10} value={0} className={["two-column"]} textId="zero"
+                    <NumberButton key={10} value={'0'} className={["two-column"]} textId="zero"
                                   clickListener={this.addNumber}/>
                     <NumberButton key="." value="." textId="decimal" clickListener={this.addDecimal}/>
                 </header>
